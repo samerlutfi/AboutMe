0998f5f63d8807c833d173a6dc05244c660bcc3f
samerlutfi <samerlutfi96@gmial.com>
2021-06-26 18:12:53 +0300

    proplems fixed

--- js/app.js
+++ js/app.js
@@ -1,112 +1,112 @@
 'use strict';
-let score= 0;
+let score = 0;
 let userName = prompt('what\'s your name?');
 console.log(userName);
 alert('Hi ' + userName + ' welcome to my webisite ');
-function reask() {
+function entrygamefun() {
   let q1 = prompt('do you want to play a simple guessing game about me?(y/n)').toLowerCase();
   switch (q1) {
-  case 'y':
+  case 'y', 'yes':
     //   console.log('nice let\'s get started!');
     alert('nice let\'s get started!');
     break;
-  case 'n':
+  case 'n', 'no':
     //   console.log('it\'s ok nice to meet you!');
     alert('it\'s ok nice to meet you');
     break;
   default:
     alert('please answer with y or n');
-    reask();
+    entrygamefun();
     break;
   }
 }
-reask();
+entrygamefun();
 
-function reask1() {
+function nationalityfun() {
 
   let nation = prompt('am i jordanian?(y/n)').toLowerCase();
   switch (nation) {
-  case 'y':
+  case 'y', 'yes':
     //   console.log('it would be my pleasure.but no im syrian');
     alert('it would be my pleasure.but no im syrian');
     break;
-  case 'n':
+  case 'n', 'no':
     //   console.log('excellent.im syrian');
     alert('excellent.im syrian');
     score++;
     break;
   default:
     alert('please answer with y or n');
-    reask1();
+    nationalityfun();
     break;
   }
 }
-reask1();
+nationalityfun();
 
-function reask2() {
+function studyfun() {
   let study = prompt('Have i finished my bachelor degree?(y/n)').toLowerCase();
   switch (study) {
-  case 'y':
+  case 'y', 'yes':
     //   console.log('that\'s right! keep going');
     alert('that\'s right! keep going');
     score++;
     break;
-  case 'n':
+  case 'n', 'no':
     //   console.log('wrong answer!i have finshed my study actualy');
     alert('wrong answer!i have finshed my study actualy');
     break;
   default:
     alert('please answer with y or n');
-    reask2();
+    studyfun();
     break;
   }
 }
-reask2();
+studyfun();
 
-function reask3() {
+function petsfun() {
   let pet = prompt('Do i like pets?(y/n)').toLowerCase();
   switch (pet) {
-  case 'y':
+  case 'y', 'yes':
     //   console.log('that\'s right!');
     alert('that\'s right!');
     score++;
     break;
-  case 'n':
+  case 'n', 'no':
     //   console.log('nope sorry.i love pets');
     alert('nope sorry.i love pets');
     break;
   default:
     alert('please answer with y or n');
-    reask3();
+    petsfun();
     break;
   }
 }
-reask3();
-function reask4() {
+petsfun();
+function foodfun() {
   let food = prompt('do i like fast food?(y/n)').toLowerCase();
   switch (food) {
-  case 'y':
+  case 'y', 'yes':
     //   console.log('that\'s right especially burgerrz');
     alert('that\'s right especially burgerrz');
     score++;
     break;
-  case 'n':
+  case 'n', 'no':
     //   console.log('it\'s ok nice to meet you');
     alert('it\'s ok nice to meet you');
     break;
   default:
     alert('please answer with y or n');
-    reask4();
+    foodfun();
     break;
   }
 }
-reask4();
+foodfun();
 // alert('that\'s all nice to meet you '+ userName +' have a lovely day');
 
 
 
 let answer = 8;
-function reask5() {
+function guessNum() {
   for (let i = 1; i <= 4; i++) {
     let guess = Number(prompt('guess my lucky number from 1 to 10'));
     if (guess > answer) {
@@ -121,15 +121,15 @@ function reask5() {
   }
   alert('the correct answer is 8');
 }
-reask5();
+guessNum();
 
 
-function reask6() {
+function favCar() {
   let cars = ['kia', 'audi', 'bmw', 'fiat'];
   for (let attempt = 1; attempt <= 6; attempt++) {
     let answer2 = prompt('can you guess one my fav car types?').toLocaleLowerCase();
     for (let i = 0; i < cars.length; i++) {
-      if (answer2 == cars[i]) {
+      if (answer2 === cars[i]) {
         alert('thats right');
         score++;
         attempt = 9;
@@ -138,5 +138,5 @@ function reask6() {
     }
   }
 }
-reask6();
-alert('your final score is '+score+' out of 6 thank you for playing');
+favCar();
+alert('your final score is ' + score + ' out of 6 thank you for playing');
